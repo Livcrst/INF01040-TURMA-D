64bd27c0d2c05d4071d341476fcccfed335c5324
Lia <livcristinna@gmail.com>
2023-12-08 20:53:31 -0300

    Lab09

--- LAB09.py
+++ LAB09.py
@@ -1,35 +1,39 @@
 #Ler a entrada completa
 Phrase = input().lower()
 CountFrequency = {}
+PhraseClear =''
 #Desconsiderar espaços em brancos, pontuações e numeros
 for caracter in Phrase:
     if caracter.isalpha():
         CountFrequency[caracter] =''
-# Contar ocorrencias
+        PhraseClear += caracter
+# Contar ocorrencias de todoos os elementos
 for element in CountFrequency:
     count = 0
     for caracter in Phrase:
         if caracter == element:
             count += 1
     CountFrequency[element] = count
-print(CountFrequency)
+#Contar ocorrencia de vogais
+vowels = ['a','e','i','o','u']
+contvowels = 0
+for letter in Phrase:
+    for vowel in vowels:
+        if letter == vowel:
+            contvowels += 1
+consoants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+contcons = 0
+for letter in Phrase:
+    for cons in consoants:
+        if letter == cons:
+            contcons += 1
 
 # Calcular frequencias
+print ("Letra\tOcorrência\tFrequência")
+for chave in sorted(CountFrequency):
+    frequency = CountFrequency[chave]/len(PhraseClear)
+    print('{}\t{}\t\t{:.2f}'.format(chave,CountFrequency[chave],frequency))
 
-#Mostrar todas as letras da frase, numero de ocorrencia e frequencia de ocorrencias
-#Mostrar frequencia de vogais
-#Mostrar frequencia de consoantes
+print('Frequência de vogais: {:.2f}'.format(contvowels/len(PhraseClear))) #Fraquencia vogais
+print('Frequência de consoantes: {:.2f}'.format(contcons/len(PhraseClear))) #Frequencia consoantes
 
-
-# def countletter(letter, word):
-#     count = 0
-#     for caracter in word:
-#         if letter == caracter:
-#             count += 1
-#     return count
-
-# for caracter in Phrase:
-#     if caracter.isalpha():
-#         CountFrequency[caracter] = countletter(caracter, Phrase)
-# print('com função')
-# print(CountFrequency)
